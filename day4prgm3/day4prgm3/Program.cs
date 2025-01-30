c9bdff43131253216360cc0891aceeba778d2d7d
Police-Hemanth-Reddy <137410903+Police-Hemanth-Reddy@users.noreply.github.com>
2025-01-30 15:32:15 +0530

    Add files via upload

---
+++ day4prgm3/day4prgm3/Program.cs
@@ -0,0 +1,13 @@
+﻿using Abstract;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Flightfare f = new Flightfare() { flightno = 11, flightname = "airindia", durationhrs = 6, des = "internation" };
+        f.flightdetails();
+        f.faredetails();
+        Flights ff = new Flightfare() { flightno = 12, flightname = "Spicejet", durationhrs = 3,des = "National" };
+        ff.flightdetails();
+        ff.faredetails();
+    }
+}
