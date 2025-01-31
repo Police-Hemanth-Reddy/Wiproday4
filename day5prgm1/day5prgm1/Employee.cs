4fc4a7403d2f2dbb610b0f145c14905527ecad05
Police-Hemanth-Reddy <137410903+Police-Hemanth-Reddy@users.noreply.github.com>
2025-01-31 11:13:12 +0530

    Add files via upload

---
+++ day5prgm1/day5prgm1/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day5prgm1
+{
+    internal interface Employee
+    {
+        void basicdetails();
+        void salarydetails();
+    }
+    interface Interface2
+    {
+        void meeting();
+    }
+    class PermanentEmployee : Employee,Interface2
+    {
+        public int emp_id { get; set; }
+        public string emp_name { get; set; }
+        public decimal salary { get; set; }
+        public void basicdetails()
+        {
+            Console.WriteLine(emp_id + " " + emp_name);
+        }
+        public void salarydetails()
+        {
+            Console.WriteLine(salary);
+        }
+        public void meeting()
+        {
+            Console.WriteLine("attended meeting");
+        }
+    }
+}
---
+++ day5prgm1/day5prgm1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace day5prgm1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the emp details:");
+            PermanentEmployee p = new PermanentEmployee() { emp_id = 111, emp_name = "hemanth", salary = 2000 };
+            p.basicdetails();
+            p.salarydetails();
+            p.meeting();
+        }
+    }
+}
