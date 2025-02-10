59b33ec6e3b0edcfece185d1b628096d93384bb0
Police-Hemanth-Reddy <137410903+Police-Hemanth-Reddy@users.noreply.github.com>
2025-02-10 15:10:48 +0530

    Add files via upload

---
+++ SOLID/SRP.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        ReportGenerator generator = new ReportGenerator();
+        ReportSaver saver = new ReportSaver();
+
+        string report = generator.GenerateReport();
+
+        saver.SaveReport(report);
+    }
+}
+
+public class ReportGenerator
+{
+    public string GenerateReport()
+    {
+        // Logic to generate the report
+        Console.WriteLine("Generating report...");
+        return "Report Data";  
+    }
+}
+
+public class ReportSaver
+{
+    public void SaveReport(string report)
+    {
+        // Logic to save the report
+        Console.WriteLine("Saving report: " + report);
+    }
+}
